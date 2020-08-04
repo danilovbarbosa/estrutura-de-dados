b822158bdbdaecf25bb0241a8dc9f5d35050c1d4
Danilo Costa <danilo.barbosa@fapce.edu.br>
2020-08-04 20:50:40 -0300

    Add 7 codes.

---
+++ uri_online_judge/problem_1004.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+  int valor1, valor2;
+
+  scanf("%d", &valor1);
+  scanf("%d", &valor2);
+
+  int PROD = valor1 * valor2;
+  printf("PROD = %d\n", PROD);
+ 
+  return 0;
+}
